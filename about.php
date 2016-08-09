9293889a0b2f367ea39866888ad50a56c0f5d350
Carlton Freeman <user@RSG4133manMBAir.hsd1.ga.comcast.net>
2016-08-08 23:26:03 -0400

    Fixed relative links to stylesheets for the 'About' page

--- about.php
+++ about.php
@@ -8,8 +8,8 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 <script async="" src="https://www.google-analytics.com/analytics.js"></script><script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 <link href="http://fonts.googleapis.com/css?family=Lato:400,700" rel="stylesheet" type="text/css">
-<link rel="stylesheet" type="text/css" href="topten.css">
-<link rel="stylesheet" type="text/css" href="hover.css">
+<link rel="stylesheet" type="text/css" href="./CSS/topten.css">
+<link rel="stylesheet" type="text/css" href="./CSS/hover.css">
 <link rel="icon" 
       type="image/png" 
       href="https://i.imgsafe.org/4c69c8c1a8.png">
