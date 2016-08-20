221ffbeb7c63a3ae37040793826bc7ba436e6bea
Zachary Butts <zacharydbutts@gmail.com>
2016-08-20 01:00:22 +0000

    Switched two companies
    Commented out the review portions until they are built

--- mobileView.php
+++ mobileView.php
@@ -17,10 +17,8 @@
                 <li><strong>Monthly options:</strong> 1 and 3 years</li>
                 <li><strong>Years in Business:</strong> 14</li>
                 <li><strong>Cancellation Fee:</strong> $50</li>
-                <li class="reviewholder">
-                    <a href="http://www.bbb.org/atlanta/business-reviews/home-warranty-plans/afc-home-club-in-marietta-ga-27282410" target="_blank">
-                    <img class="bbb" src="http://theoverheaddoordepot.com/wp-content/uploads/BBB-Logo-Blue-Horizontal.png?quality=100.3015112611140" alt="BBB Logo"><br><h4>B BBB Rating<br>Click for Review</h4></a>
-                </li>
+                <!--<li><strong>Rating: 9.9/10 <br />
+                    <a href="/reviews/afc.php">Read Review</a></strong></li>!-->
               </ul>
               <a itemprop="url" href="http://www.afchomeclub.com/home-warranty-free-quote-BOHW.php" target="_blank"><button class="quote hvr-grow" onclick=_gaq.push([‘_trackEvent’, ‘button’, ‘clicked’ ‘AFC’]);>Visit Site</button></a>
               </div>   
@@ -42,8 +40,9 @@
                 <li><strong>Monthly options:</strong> 1 year plan only</li>
                 <li><strong>Years in Business:</strong> 8</li>
                 <li><strong>Cancellation Fee:</strong> $50</li>
-                <li class="reviewholder"><a href="https://www.consumeraffairs.com/homeowners/choice_home_warranty.html" target="_blank"><img class="consumeraffair" src="https://media.consumeraffairs.com/static/img/common/trusted-shield-102.036a65cca665.png" alt="Consumer Affairs"><br><h4>Consumer Affairs Accredited<br>Click for Review</h4></a></li>
-              </ul> 
+                <!--<li><strong>Rating: 9.9/10 <br />
+                    <a href="/reviews/afc.php">Read Review</a></strong></li>!-->
+                </ul> 
             <a itemprop="url" href="http://chwplan.com/a/a54/index.php?utm_source=bohwfm&amp;utm_campaign=SUBID" target="_blank"><button class="quote hvr-grow">Visit Site</button></a>
             </div> 
         </div>
@@ -59,106 +58,16 @@
                 <li><strong>Work Guarantee:</strong> 90 days parts/labor</li>
                 <li><strong>Availability:</strong> Nationwide</li>
                 <li><strong>Can choose your own service tech?</strong> No</li>
-                <li><strong>Average response time:</strong> 24-48 hours<br><br></li>
+                <li><strong>Average response time:</strong> 24-48 hours</li>
                 <li><strong>Pricing locked in?</strong> No</li>
                 <li><strong>Length of policies:</strong> 1 / 3 / 5 years</li>
                 <li><strong>Monthly options:</strong> 1 year plan only</li>
                 <li><strong>Years in Business:</strong> 4</li>
                 <li><strong>Cancellation Fee:</strong> $75</li>
-                <li class="reviewholder"><a href="https://www.consumeraffairs.com/homeowners/delta-home-protect.html" target="_blank"><img class="consumeraffair" src="https://media.consumeraffairs.com/static/img/common/trusted-shield-102.036a65cca665.png" alt="Consumer Affairs Logo"><br><h4>Consumer Affairs Accredited<br>Click for Review</h4></a></li>
-              </ul> 
+                <!--<li><strong>Rating: 9.9/10 <br />
+                    <a href="/reviews/afc.php">Read Review</a></strong></li>!-->
+            </ul> 
             <a itemprop="url" href="https://selecthomewarranty.com/affiliate/3c0c53" target="_blank"><button class="quote hvr-grow">Visit Site</button></a>
-            </div>
-            
-          <div itemscope itemtype="http://schema.org/Organization" target="contact" class="colrow col-xs-6">
-                <div class="company-logo totalprotect">
-                <img itemprop="logo" src="https://media.consumeraffairs.com/files/cache/guide-campaign-logos/total_protect_logo_thumbnail.jpg" alt="Total Protect Logo" id="tp-mobile-logo">
-                </div>
-            <ul>
-                <li><strong>Waiting Period:</strong> 15 days</li>
-                <li><strong>Service Fee:</strong> $75/100/125</li>
-                <li><strong>Work Guarantee:</strong> 6 months parts/labor</li>
-                <li><strong>Availability:</strong> Nationwide</li>
-                <li><strong>Can choose your own service tech?</strong> No</li>
-                <li><strong>Average response time:</strong> 2 hours emergency / 48 hours</li>
-                <li><strong>Pricing locked in?</strong> No</li>
-                <li><strong>Length of policies:</strong> 1 year plans</li>
-                <li><strong>Monthly options:</strong> 1 year plan only</li>
-                <li><strong>Years in Business:</strong> 33</li>
-                <li><strong>Cancellation Fee:</strong> $25</li>
-                <li class="reviewholder">
-                    <a href="http://www.bbb.org/south-east-florida/business-reviews/home-warranty-plans/homesure-of-america-in-sunrise-fl-4004196" target="_blank">
-                    <img class="bbb" src="http://theoverheaddoordepot.com/wp-content/uploads/BBB-Logo-Blue-Horizontal.png?quality=100.3015112611140" alt="BBB Logo"><br><h4>B+ BBB Rating<br>Click for Review</h4></a>
-                </li>
-              </ul>
-                <a itemprop="url" href="http://www.anrdoezrs.net/click-8083462-11382333-1453493377000" target="_top"><button class="quote hvr-grow">Visit Site</button></a>
-            </div>  
-    </div>
-<!--BEGIN SECOND ROW !-->
-        <div class="row">
-
-          <div itemscope itemtype="http://schema.org/Organization" target="contact" class="colrow col-xs-6">
-            <div class="company-logo secure">
-                <img itemprop="logo" src="http://securehomewarranty.com/images/brand/SecureHomeWarranty_Logo_V2_400w_trimmed.jpg" alt="Secure Home Warranty Logo" id ="secure-mobile-logo">
-            </div>
-            <ul>
-                <li><strong>Waiting Period:</strong> 30 days</li>
-                <li><strong>Service Fee:</strong> $65</li>
-                <li><strong>Work Guarantee:</strong> 90 days </li>
-                <li><strong>Availability:</strong> Nationwide</li>
-                <li><strong>Can choose your own service tech?</strong> No</li>
-                <li><strong>Average response time:</strong> 24-48 hours</li>
-                <li><strong>Pricing locked in?</strong> Yes</li>
-                <li><strong>Length of policies:</strong> 1 year plans</li>
-                <li><strong>Monthly options:</strong> 1 year plan only</li>
-                <li><strong>Years in Business:</strong> 10</li>
-                <li><strong>Cancellation Fee:</strong> $0</li>
-                <li class="reviewholder"><a href="http://securehomewarranty.com/" target="_blank"> <img class="consumeraffair" src="https://media.consumeraffairs.com/static/img/common/trusted-shield-102.036a65cca665.png" alt="Consumer Affairs Logo"><br><h4>Consumer Affairs Accredited<br></h4></a></li>
-              </ul> 
-            <a itemprop="url" href="http://securehomewarranty.com/" target='_blank'><button class="quote hvr-grow">Visit Site</button></a>
-            </div>  
-
-          <div itemscope itemtype="http://schema.org/Organization" target="contact" class="colrow col-xs-6">
-                <div class="company-logo">
-                <img itemprop="logo" src="https://media.consumeraffairs.com/files/logos/first-american-home-buyers-protection_logo_2521.png" alt="First American Logo" id="fa-mobile-logo">
-                </div>
-            <ul>
-                <li><strong>Waiting Period:</strong> 30 days</li>
-                <li><strong>Service Fee:</strong> $65</li>
-                <li><strong>Work Guarantee:</strong> 30 days parts/labor</li> 
-                <li><strong>Availability:</strong> Nationwide</li>
-                <li><strong>Can choose your own service tech?</strong> No</li>
-                <li><strong>Average response time:</strong> 24-48 hours</li>
-                <li><strong>Pricing locked in?</strong> No</li>
-                <li><strong>Length of policies:</strong> 1 year plans only</li>
-                <li><strong>Monthly options:</strong> 1 year plans only</li>
-                <li><strong>Years in business:</strong> 32</li>
-                <li><strong>Cancellation Fee:</strong> $50</li>
-                <li class="reviewholder"><a href="http://www.bbb.org/losangelessiliconvalley/business-reviews/home-warranty-plans/first-american-home-buyers-protection-corporation-in-canoga-park-ca-18779" target="_blank"><img class="bbb" src="http://theoverheaddoordepot.com/wp-content/uploads/BBB-Logo-Blue-Horizontal.png?quality=100.3015112611140" alt="BBB Logo"><br><h4>B BBB Rating<br>Click for Review</h4></a>
-                </li>
-              </ul>   
-                <a itemprop="url" href="https://homewarranty.firstam.com/" target="_blank"><button class="quote hvr-grow" onclick=_gaq.push([‘_trackEvent’, ‘button’, ‘clicked’ ‘FAHW’]);>Visit Site</button></a>
-            </div> 
-            
-          <div itemscope itemtype="http://schema.org/Organization" target="contact" class="colrow col-xs-6">
-            <div class="company-logo">
-                <img itemprop="logo" src="http://www.deltahomeprotect.com/wp-content/uploads/2015/06/delta-home-protect-retina.png" alt="Delta Home Protect Logo" id="dhp-mobile-logo">
-            </div>
-            <ul>
-                <li><strong>Waiting Period:</strong> 30 days</li>
-                <li><strong>Service Fee:</strong> $45</li>
-                <li><strong>Work Guarantee:</strong> 90 days parts/labor</li>
-                <li><strong>Availability:</strong> Nationwide</li>
-                <li><strong>Can choose your own service tech?</strong> No</li>
-                <li><strong>Average response time:</strong> 24-48 hours</li>
-                <li><strong>Pricing locked in?</strong> No</li>
-                <li><strong>Length of policies:</strong> 1 / 3 / 5 years</li>
-                <li><strong>Monthly options:</strong> 1 year plan only</li>
-                <li><strong>Years in Business:</strong> 2</li>
-                <li><strong>Cancellation Fee:</strong> $50</li>
-                <li class="reviewholder"><a href="https://www.consumeraffairs.com/homeowners/delta-home-protect.html" target="_blank"><img class="consumeraffair" src="https://media.consumeraffairs.com/static/img/common/trusted-shield-102.036a65cca665.png" alt="Consumer Affairs Logo"><br><h4>Consumer Affairs Accredited<br>Click for Review</h4></a></li>
-              </ul> 
-            <a itemprop="url" href="http://www.deltahomeprotect.com/" target="_blank"><button class="quote hvr-grow">Visit Site</button></a>
             </div>
             
           <div itemscope itemtype="http://schema.org/Organization" target="contact" class="colrow col-xs-6" id="ahs-mobile-col">
@@ -177,11 +86,101 @@
                 <li><strong>Monthly options:</strong> 1 year plan only</li>
                 <li><strong>Years in Business:</strong> 45</li>
                 <li><strong>Cancellation Fee:</strong> None</li>
-                <li class="reviewholder">
-                    <a href="http://www.bbb.org/memphis/business-reviews/real-estate-maintenance-protection-plan/american-home-shield-in-memphis-tn-22001027" target="_blank">
-                    <img class="bbb" src="http://theoverheaddoordepot.com/wp-content/uploads/BBB-Logo-Blue-Horizontal.png?quality=100.3015112611140" alt="BBB Logo"><br><h4>B BBB Rating<br>Click for Review</h4></a>
-                </li>
+                <!--<li><strong>Rating: 9.9/10 <br />
+                    <a href="/reviews/afc.php">Read Review</a></strong></li>!-->
               </ul> 
                 <a itemprop="url" href="https://www.ahs.com/" target="_blank"><button class="quote hvr-grow" onclick=_gaq.push([‘_trackEvent’, ‘button’, ‘clicked’ ‘AHS’]);>Visit Site</button></a>
+            </div>   
+    </div>
+<!--BEGIN SECOND ROW !-->
+        <div class="row">
+
+          <div itemscope itemtype="http://schema.org/Organization" target="contact" class="colrow col-xs-6">
+            <div class="company-logo secure">
+                <img itemprop="logo" src="http://securehomewarranty.com/images/brand/SecureHomeWarranty_Logo_V2_400w_trimmed.jpg" alt="Secure Home Warranty Logo" id ="secure-mobile-logo">
+            </div>
+            <ul>
+                <li><strong>Waiting Period:</strong> 30 days</li>
+                <li><strong>Service Fee:</strong> $65</li>
+                <li><strong>Work Guarantee:</strong> 90 days<br><br></li>
+                <li><strong>Availability:</strong> Nationwide</li>
+                <li><strong>Can choose your own service tech?</strong> No</li>
+                <li><strong>Average response time:</strong> 24-48 hours</li>
+                <li><strong>Pricing locked in?</strong> Yes</li>
+                <li><strong>Length of policies:</strong> 1 year plans</li>
+                <li><strong>Monthly options:</strong> 1 year plan only</li>
+                <li><strong>Years in Business:</strong> 10</li>
+                <li><strong>Cancellation Fee:</strong> $0</li>
+                <!--<li><strong>Rating: 9.9/10 <br />
+                    <a href="/reviews/afc.php">Read Review</a></strong></li>!-->
+                </ul> 
+            <a itemprop="url" href="http://securehomewarranty.com/" target='_blank'><button class="quote hvr-grow">Visit Site</button></a>
+            </div>  
+
+          <div itemscope itemtype="http://schema.org/Organization" target="contact" class="colrow col-xs-6" id="firstam-mobile-col">
+                <div class="company-logo">
+                <img itemprop="logo" src="https://media.consumeraffairs.com/files/logos/first-american-home-buyers-protection_logo_2521.png" alt="First American Logo" id="fa-mobile-logo">
+                </div>
+            <ul>
+                <li><strong>Waiting Period:</strong> 30 days</li>
+                <li><strong>Service Fee:</strong> $65</li>
+                <li><strong>Work Guarantee:</strong> 30 days parts/labor</li> 
+                <li><strong>Availability:</strong> Nationwide</li>
+                <li><strong>Can choose your own service tech?</strong> No</li>
+                <li><strong>Average response time:</strong> 24-48 hours</li>
+                <li><strong>Pricing locked in?</strong> No</li>
+                <li><strong>Length of policies:</strong> 1 year plans only</li>
+                <li><strong>Monthly options:</strong> 1 year plans only</li>
+                <li><strong>Years in business:</strong> 32</li>
+                <li><strong>Cancellation Fee:</strong> $50</li>
+                <!--<li><strong>Rating: 9.9/10 <br />
+                    <a href="/reviews/afc.php">Read Review</a></strong></li>!-->
+              </ul>   
+                <a itemprop="url" href="https://homewarranty.firstam.com/" target="_blank"><button class="quote hvr-grow" onclick=_gaq.push([‘_trackEvent’, ‘button’, ‘clicked’ ‘FAHW’]);>Visit Site</button></a>
+            </div> 
+</div>
+    <div class="row">
+          <div itemscope itemtype="http://schema.org/Organization" target="contact" class="colrow col-xs-6" id="dhp-mobile-col">
+            <div class="company-logo">
+                <img itemprop="logo" src="http://www.deltahomeprotect.com/wp-content/uploads/2015/06/delta-home-protect-retina.png" alt="Delta Home Protect Logo" id="dhp-mobile-logo">
+            </div>
+            <ul>
+                <li><strong>Waiting Period:</strong> 30 days</li>
+                <li><strong>Service Fee:</strong> $45</li>
+                <li><strong>Work Guarantee:</strong> 90 days parts/labor</li>
+                <li><strong>Availability:</strong> Nationwide</li>
+                <li><strong>Can choose your own service tech?</strong> No</li>
+                <li><strong>Average response time:</strong> 24-48 hours</li>
+                <li><strong>Pricing locked in?</strong> No</li>
+                <li><strong>Length of policies:</strong> 1 / 3 / 5 years</li>
+                <li><strong>Monthly options:</strong> 1 year plan only</li>
+                <li><strong>Years in Business:</strong> 2</li>
+                <li><strong>Cancellation Fee:</strong> $50</li>
+                <!--<li><strong>Rating: 9.9/10 <br />
+                    <a href="/reviews/afc.php">Read Review</a></strong></li>!-->
+                </ul> 
+            <a itemprop="url" href="http://www.deltahomeprotect.com/" target="_blank"><button class="quote hvr-grow">Visit Site</button></a>
+            </div>
+            
+x          <div itemscope itemtype="http://schema.org/Organization" target="contact" class="colrow col-xs-6">
+                <div class="company-logo totalprotect">
+                <img itemprop="logo" src="https://media.consumeraffairs.com/files/cache/guide-campaign-logos/total_protect_logo_thumbnail.jpg" alt="Total Protect Logo" id="tp-mobile-logo">
+                </div>
+            <ul>
+                <li><strong>Waiting Period:</strong> 15 days</li>
+                <li><strong>Service Fee:</strong> $75/100/125</li>
+                <li><strong>Work Guarantee:</strong> 6 months parts/labor</li>
+                <li><strong>Availability:</strong> Nationwide</li>
+                <li><strong>Can choose your own service tech?</strong> No</li>
+                <li><strong>Average response time:</strong> 2 hours emergency / 48 hours</li>
+                <li><strong>Pricing locked in?</strong> No</li>
+                <li><strong>Length of policies:</strong> 1 year plans</li>
+                <li><strong>Monthly options:</strong> 1 year plan only</li>
+                <li><strong>Years in Business:</strong> 33</li>
+                <li><strong>Cancellation Fee:</strong> $25</li>
+                <!--<li><strong>Rating: 9.9/10 <br />
+                    <a href="/reviews/afc.php">Read Review</a></strong></li>!-->          
+                </ul>
+                <a itemprop="url" href="http://www.anrdoezrs.net/click-8083462-11382333-1453493377000" target="_top"><button class="quote hvr-grow">Visit Site</button></a>
             </div> 
     </div>
