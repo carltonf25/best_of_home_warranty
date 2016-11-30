adb2f5f567236c4bf48a2f8c86b97793653fa47e
Zachary Butts <zacharydbutts@gmail.com>
2016-11-30 14:05:53 -0500

    Update deskTopView.php

--- deskTopView.php
+++ deskTopView.php
@@ -1,11 +1,11 @@
-       <div class="row">
-          <div itemscope itemtype="http://schema.org/Organization" target="contact" class="colrow col-md-3">    
-              <button src="javascript:void(0)" class="ribbon" id="afcbutton" onclick ="displayPromo(this.id); ga('send', 'event', 'promoRibbon', 'promoClick', 'AFC');">Click to View Promotions</button>
+<div class="row">
+          <div itemscope itemtype="http://schema.org/Organization" target="contact" class="colrow col-md-3">   
+              <button src="javascript:void(0)" class="ribbon" id="afcbutton" onclick = "displayPromo(this.id); ga('send', 'event', 'promoRibbon', 'promoClick', 'AFC');">Click to View Promotions</button>
               <span class="endtop"></span><span class="endbottom"></span>
             <div class="company-logo">
               <img itemprop="logo" src="http://home-warranty-company-reviews.com/wp-content/uploads/2015/01/AFC-Home-Club-Logo_Stacked-360x203.jpg" alt="AFC Logo">
               </div>
-              <ul>
+       <ul>
                 <li><strong>Waiting Period:</strong> 30 days</li>
                 <li><strong>Service Fee: </strong> $60</li>
                 <li><strong>Work Guarantee:</strong> Policy lifetime parts/labor</li>
@@ -17,11 +17,13 @@
                 <li><strong>Monthly options:</strong> 1 or 3 year</li>
                 <li><strong>Years in Business:</strong> 14</li>
                 <li><strong>Cancellation Fee:</strong> $50</li>
-                <li><strong>
-                   <a href="./reviews/afc.php">Read Review</a></strong>
+                <li class="reviewholder">
+                    <a href="http://www.bbb.org/atlanta/business-reviews/home-warranty-plans/afc-home-club-in-marietta-ga-27282410" target="_blank">
+                    <img class="bbb" src="http://theoverheaddoordepot.com/wp-content/uploads/BBB-Logo-Blue-Horizontal.png?quality=100.3015112611140" alt="bbb_logo"><br><h4>B BBB Rating<br>Click for Review</h4></a>
                 </li>
               </ul>
-              <a itemprop="url" href="http://www.afchomeclub.com/home-warranty-free-quote-BOHW.php" target="_blank"><button class="quote hvr-grow" onclick="ga('send', 'event', 'visitSite', 'click', 'AFC');">Visit Site</button></a>
+              <a itemprop="url" href="http://www.afchomeclub.com/home-warranty-free-quote-BOHW.php" target="_blank"><button class="quote hvr-grow" onclick="ga('send', 'event', 'visitSite', 'click', 'AFC');">Visit Site</button></a><br/>
+              <a href="http://www.afchomeclub.com/home-warranty-free-quote-BOHW.php">(678)-236-0646</a>
               </div>   
             
         <div itemscope itemtype="http://schema.org/Organization" class="colrow col-md-3">
@@ -33,19 +35,20 @@
                 <li><strong>Waiting Period:</strong> 30 days</li>
                 <li><strong>Service Fee: </strong> $60</li>
                 <li><strong>Work Guarantee:</strong> 90 days parts/labor</li>
+                
                 <li><strong>Availability:</strong> Nationwide</li>
-                 <li><strong>Can choose your own service tech?</strong> No</li>
+                 <li><strong>Can choose your own service tech?</strong>Yes</li>
                 <li><strong>Average response time:</strong> 24-48 hours</li>
                 <li><strong>Pricing locked in?</strong> No</li>
                 <li><strong>Length of policies:</strong> 1 / 3 / 5 year</li>
-                <li><strong>Monthly options:</strong> 1 year plans</li>
+                <li><strong>Monthly options:</strong> 1 year plan only</li>
                 <li><strong>Years in Business:</strong> 8</li>
                 <li><strong>Cancellation Fee:</strong> $50</li>
-                <li><strong>
-                   <a href="./reviews/chw.php">Read Review</a></strong>
-                </li>
+                <li class="reviewholder">
+                    <img class="consumeraffair" src="https://media.consumeraffairs.com/static/img/common/trusted-shield-102.036a65cca665.png" alt="ca_logo"><h4><br>Consumer Affairs Accredited</h4></li>
               </ul> 
-            <a itemprop="url" href="http://chwplan.com/a/a54/index.php?utm_source=bohwfm&amp;utm_campaign=SUBID" target="_blank"><button class="quote hvr-grow" onclick="ga('send', 'event', 'visitSite', 'click', 'Choice');">Visit Site</button></a>
+            <a itemprop="url" href="http://chwplan.com/a/a54/index.php?utm_source=bohwfm&amp;utm_campaign=SUBID" target="_blank"><button class="quote hvr-grow" onclick="ga('send', 'event', 'visitSite', 'click', 'Choice');">Visit Site</button></a><br/>
+            <a href="http://chwplan.com/a/a54/index.php?utm_source=bohwfm&amp;utm_campaign=SUBID">(800)-816-1053</a>
             </div> 
             
         <div itemscope itemtype="http://schema.org/Organization" class="colrow col-md-3">
@@ -61,39 +64,39 @@
                 <li><strong>Can choose your own service tech?</strong>Yes</li>
                 <li><strong>Average response time:</strong> 24-48 hours</li>
                 <li><strong>Pricing locked in?</strong> No</li>
-                <li><strong>Length of policies:</strong> 1 / 3 / 5 year</li>
-                <li><strong>Monthly options:</strong> 1 year plans</li>
+                <li><strong>Length of policies:</strong> 1 / 3 / 5 years</li>
+                <li><strong>Monthly options:</strong> 1 year plan only</li>
                 <li><strong>Years in Business:</strong> 4</li>
                 <li><strong>Cancellation Fee:</strong> $75</li>
-                <li><strong>
-                    <a href="./reviews/shw.php">Read Review</a></strong></li>
-                </ul> 
-            <a itemprop="url" href="https://selecthomewarranty.com/affiliate/3c0c53" target="_blank"><button class="quote hvr-grow" onclick="ga('send', 'event', 'visitSite', 'click', 'Select');">Visit Site</button></a>
+                <li class="reviewholder">
+                    <img class="consumeraffair" src="https://media.consumeraffairs.com/static/img/common/trusted-shield-102.036a65cca665.png" alt="Consumer Affairs Logo"><h4><br>Consumer Affairs Accredited</h4></li>
+              </ul> 
+            <a itemprop="url" href="https://selecthomewarranty.com/affiliate/3c0c53" target="_blank"><button class="quote hvr-grow" onclick="ga('send', 'event', 'visitSite', 'click', 'Select');">Visit Site</button></a><br/>
+            <a href="https://selecthomewarranty.com/affiliate/3c0c53">855-610-4663</a>
             </div>
             
             <div itemscope itemtype="http://schema.org/Organization" class="colrow col-md-3">
-              <button src="javascript:void(0)" class="ribbon" id="totalbutton" onclick = "displayPromo(this.id); ga('send', 'event', 'promoRibbon', 'promoClick', 'Total');">Click to View Promotions</button>
-              <span class="endtop"></span><span class="endbottom"></span>
-                <div class="company-logo totalprotect">
-                <img itemprop="logo" id="totalprotect" src="https://media.consumeraffairs.com/files/cache/guide-campaign-logos/total_protect_logo_thumbnail.jpg" alt="Total Protect Logo">
+                <div class="company-logo">
+                <img itemprop="logo" src="http://jlspartnerconnection.com/wp-content/uploads/sites/4/2014/07/AHS-Logo-new.jpg" alt="American Home Shield Logo">
                 </div>
             <ul>
                 <li><strong>Waiting Period:</strong> 15 days</li>
                 <li><strong>Service Fee:</strong> $75, $100, $125</li>
-                <li><strong>Work Guarantee:</strong> 6 months parts/labor</li>
+                <li><strong>Work Guarantee:</strong> Depends on technician</li>
                 <li><strong>Availability:</strong> Nationwide</li>
-                <li><strong>Can choose your own service tech?</strong> No</li>
-                <li><strong>Average response time:</strong> 2 hours emergency / 48 hours</li>
+                 <li><strong>Can choose your own service tech?</strong> No</li>
+                <li><strong>Average response time:</strong> 24-48 hours</li>
                 <li><strong>Pricing locked in?</strong> No</li>
-                <li><strong>Length of policies:</strong> 1 year</li>
-                <li><strong>Monthly options:</strong> 1 year plans</li>
-                <li><strong>Years in Business:</strong> 33</li>
-                <li><strong>Cancellation Fee:</strong> $25</li>
-                <li><strong>
-                    <a href="./reviews/total_protect.php">Read Review</a></strong></li>
-                </ul>
-              </ul>
-                <a itemprop="url" href="http://www.anrdoezrs.net/click-8083462-11382333-1453493377000" target="_top"><button class="quote hvr-grow" onclick="ga('send', 'event', 'visitSite', 'click', 'Total');">Visit Site</button></a>
+                <li><strong>Length of policies:</strong> 1 Year</li>
+                <li><strong>Monthly options:</strong> 1 year plan only</li>
+                <li><strong>Years in Business:</strong> 45</li>
+                <li><strong>Cancellation Fee:</strong> None</li>
+                <li class="reviewholder">
+                    <a href="http://www.bbb.org/memphis/business-reviews/real-estate-maintenance-protection-plan/american-home-shield-in-memphis-tn-22001027" target="_blank">
+                    <img class="bbb" src="http://theoverheaddoordepot.com/wp-content/uploads/BBB-Logo-Blue-Horizontal.png?quality=100.3015112611140" alt="BBB Logo"><br><h4>B BBB Rating<br>Click for Review</h4></a>
+                </li>
+              </ul> 
+                <a itemprop="url" href="https://www.ahs.com/" target="_blank"><button class="quote hvr-grow" onclick=_gaq.push([‘_trackEvent’, ‘button’, ‘clicked’ ‘AHS’]);>Visit Site</button></a>
             </div>
             </div>       
 <!--BEGIN SECOND ROW !-->
@@ -112,13 +115,13 @@
                 <li><strong>Average response time:</strong> 24-48 hours</li>
                 <li><strong>Pricing locked in?</strong> Yes</li>
                 <li><strong>Length of policies:</strong> 1 year plans</li>
-                <li><strong>Monthly options:</strong> 1 year plans</li>
+                <li><strong>Monthly options:</strong> 1 year plan only</li>
                 <li><strong>Years in Business:</strong> 10f</li>
                 <li><strong>Cancellation Fee:</strong> $0</li>
-                <li><strong>
-                    <a href="./reviews/secure.php">Read Review</a></strong></li>
-                </ul>
-            <a itemprop="url" href="http://securehomewarranty.com/" target='_blank'><button class="quote hvr-grow">Visit Site</button></a>
+                <li class="reviewholder">
+                    <img class="consumeraffair" src="https://media.consumeraffairs.com/static/img/common/trusted-shield-102.036a65cca665.png" alt="Consumer Affairs Logo"><h4><br>Consumer Affairs Accredited</h4></li>
+              </ul> 
+            <a itemprop="url" href="/secure/secure.html" target='_blank'><button class="quote hvr-grow">Visit Site</button></a>
             </div>
 
             <div itemscope itemtype="http://schema.org/Organization" class="colrow col-md-3">
@@ -137,14 +140,15 @@
                 <li><strong>Monthly options:</strong> 1 year plans</li>
                 <li><strong>Years in business:</strong> 32</li>
                 <li><strong>Cancellation Fee:</strong> $50</li>
-                <li><strong>
-                    <a href="./reviews/first_american.php">Read Review</a></strong></li>
-                </ul>
+                <li class="reviewholder">
+                    <a href="http://www.bbb.org/losangelessiliconvalley/business-reviews/home-warranty-plans/first-american-home-buyers-protection-corporation-in-canoga-park-ca-18779" target="_blank">
+                    <img class="bbb" src="http://theoverheaddoordepot.com/wp-content/uploads/BBB-Logo-Blue-Horizontal.png?quality=100.3015112611140" alt="BBB Logo" /><br><h4>B BBB Rating<br>Click for Review</h4></a>
+                </li>
               </ul>   
                 <a itemprop="url" href="https://homewarranty.firstam.com/" target="_blank"><button class="quote hvr-grow">Visit Site</button></a>
             </div> 
             
-        <div itemscope itemtype="http://schema.org/Organization" class="colrow col-md-3">
+        <!--<div itemscope itemtype="http://schema.org/Organization" class="colrow col-md-3">
             <div class="company-logo">
                 <img itemprop="logo" id="delta" src="http://www.deltahomeprotect.com/wp-content/uploads/2015/06/delta-home-protect-retina.png" alt="Delta Home Protect Logo">
             </div>
@@ -157,36 +161,38 @@
                 <li><strong>Average response time:</strong> 24-48 hours</li>
                 <li><strong>Pricing locked in?</strong> No</li>
                 <li><strong>Length of policies:</strong> 1 / 3 / 5 year</li>
-                <li><strong>Monthly options:</strong> 1 year plans</li>
+                <li><strong>Monthly options:</strong> 1 year plan only</li>
                 <li><strong>Years in Business:</strong> 2</li>
                 <li><strong>Cancellation Fee:</strong> $50</li>
-                <li><strong>
-                    <a href="./reviews/dhp.php">Read Review</a></strong></li>
-                </ul>
+                <li class="reviewholder">
+                    <img class="consumeraffair" src="https://media.consumeraffairs.com/static/img/common/trusted-shield-102.036a65cca665.png" alt="Consumer Affairs Logo"><h4><br>Consumer Affairs Accredited</h4></li>
+              </ul> 
             <a itemprop="url" href="http://www.deltahomeprotect.com/" target="_blank"><button class="quote hvr-grow">Visit Site</button></a>
-            </div>
+            </div>!-->
             
             <div itemscope itemtype="http://schema.org/Organization" class="colrow col-md-3">
-                <div class="company-logo">
-                <img itemprop="logo" src="http://jlspartnerconnection.com/wp-content/uploads/sites/4/2014/07/AHS-Logo-new.jpg" alt="American Home Shield Logo">
+              <button src="javascript:void(0)" class="ribbon" id="totalbutton" onclick = "displayPromo(this.id); ga('send', 'event', 'promoRibbon', 'promoClick', 'Total');">Click to View Promotions</button>
+              <span class="endtop"></span><span class="endbottom"></span>
+                <div class="company-logo totalprotect">
+                <img itemprop="logo" id="totalprotect" src="https://media.consumeraffairs.com/files/cache/guide-campaign-logos/total_protect_logo_thumbnail.jpg" alt="Total Protect Logo">
                 </div>
             <ul>
                 <li><strong>Waiting Period:</strong> 15 days</li>
                 <li><strong>Service Fee:</strong> $75, $100, $125</li>
-                <li><strong>Work Guarantee:</strong> Depends on technician</li>
+                <li><strong>Work Guarantee:</strong> 6 months parts/labor</li>
                 <li><strong>Availability:</strong> Nationwide</li>
-                 <li><strong>Can choose your own service tech?</strong> No</li>
-                <li><strong>Average response time:</strong> 24-48 hours</li>
+                <li><strong>Can choose your own service tech?</strong> No</li>
+                <li><strong>Average response time:</strong> 2 hours emergency / 48 hours</li>
                 <li><strong>Pricing locked in?</strong> No</li>
-                <li><strong>Length of policies:</strong> 1 Year</li>
-                <li><strong>Monthly options:</strong> 1 year plans</li>
-                <li><strong>Years in Business:</strong> 45</li>
-                <li><strong>Cancellation Fee:</strong> None</li>
-                <li><strong>
-                    <a href="./reviews/ahs.php">Read Review</a></strong></li>
-                </ul>
-              </ul> 
-                <a itemprop="url" href="https://www.ahs.com/" target="_blank"><button class="quote hvr-grow" onclick=_gaq.push([‘_trackEvent’, ‘button’, ‘clicked’ ‘AHS’]);>Visit Site</button></a>
-                </div> 
+                <li><strong>Length of policies:</strong> 1 year</li>
+                <li><strong>Monthly options:</strong> 1 year plan only</li>
+                <li><strong>Years in Business:</strong> 33</li>
+                <li><strong>Cancellation Fee:</strong> $25</li>
+                <li class="reviewholder">
+                    <a href="http://www.bbb.org/south-east-florida/business-reviews/home-warranty-plans/homesure-of-america-in-sunrise-fl-4004196" target="_blank">
+                    <img class="bbb" src="http://theoverheaddoordepot.com/wp-content/uploads/BBB-Logo-Blue-Horizontal.png?quality=100.3015112611140" alt="BBB Logo"><br><h4>B+ BBB Rating<br>Click for Review</h4></a>
+                </li>
+              </ul>
+                <a itemprop="url" href="http://www.anrdoezrs.net/click-8083462-11382333-1453493377000" target="_top"><button class="quote hvr-grow" onclick="ga('send', 'event', 'visitSite', 'click', 'Total');">Visit Site</button></a>
+            </div> 
             </div>
-        </div>
