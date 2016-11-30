0255cb23c2c32ea5b2c0b2440228a5df17c403f8
Zach <buttszachary@gmail.com>
2016-11-30 15:28:57 -0500

    Adding in the SEO content for CHW

--- reviews/chw.php
+++ reviews/chw.php
@@ -30,31 +30,105 @@ f<!DOCTYPE html>
 
 <!--Start of Content-->
 
-  <div class="review_heading">
-    <h1>Choice Home Warranty</h1>
-    <small>Review by: Goobie Perkins</small>
-  </div>
-
-  <div class="review_box">
-
-    <h2>TL;DR</h2>
-      <p>AFC really distinguishes itself with its customer service and in-house claim handling. They have excellent pricing and some no-frills plans that would work great for landlords with very limited investment in appliances.</p> 
-    <h2>Coverage</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    <h2>Customer Service</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
-    <h2>Overall Value</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  <div class="container review_heading">
+    <img itemprop="logo" src="http://res.cloudinary.com/goodsearch/image/upload/v1449103404/hi_resolution_merchant_logos/choice-home-warranty_coupons.png" alt="Choice Home Warranty Logo">
 
   </div>
-  <div class="author_box">
+
+  <div class="container review_box">
+      <div class="paragraph_box col-md-12">
+        <h2>Overview:</h2>
+          <p class="reviewParagraphs">Choice Home Warranty is a leading provider of cost effective home warranty plans and
+    offers unparalleled customer service. Choice is highly regarded for their commitment to
+    providing fast, efficient and affordable premiums that include basic and total coverage.</p> 
+          <p class="reviewParagraphs">They offer a no-fee, transferable warranty plan structure that make their plans ideal for
+    homeowners, landlords or realtors who may consider selling their home in the future.
+    Choice is however ideal for any existing property owner who’s looking for reliable
+    home warranty services that provides security and added value to their home</p>
+          <p class="reviewParagraphs">Another great benefit to using Choice Home Warranty’s plans is their integrated “work
+    order dispatch system” that hastens repair and maintenance services. This also ties into
+    their easily accessible, personalized home maintenance checklist that reminds the
+    homeowner of upcoming or important home considerations. Best of all, all questions
+    and concerns can be answered by their customer service call center at anytime, 24/7.</p>
+                </div>
+      <div class="col-md-6">
+          <h1>What we like: </h1>
+        <ul class="reviewParagraphs">
+          <li>First month is free, regardless of the service contract chosen</li>
+            <li>Reliable, knowledgeable and friendly 24/7 call center.</li>
+            <li>All warranty plans are 100% transferable to a future homebuyer.</li>
+            <li>Respected company in the home warranty industry.</li>
+            <li>Automated dispatch service. Think “Uber” for contractors.</li>
+          </ul>
+      </div>
+            <div class="col-md-6">
+          <h1>What we dont like: </h1>
+          <ul class="reviewParagraphs">
+            <li>Terms and conditions exclude certain unforeseeable damages like rust and
+    power surges. We advise reading the terms and conditions closely</li>
+              <li>Commercial grade or certain high-end products or systems may not be
+    completely covered, often limited to a set dollar value.</li>
+          </ul>
+      </div>
+      <div class="col-md-12">
+    <h2>Basic vs. Total Coverage: </h2>
+      <p class="reviewParagraphs">An important element to consider with Choice Home Warranty is their basic and total
+coverage options. Although their website outlines some very easy to read coverage
+tables, we feel it’s important to outline the real differences.</p>
+      </div>
+      <div class="col-md-6">
+          <h1>Basic Plan: </h1>
+        <ul class="reviewParagraphs">
+            <li>Heating System</li>
+            <li>Electrical System</li>
+            <li>Plumbing System</li>
+            <li>Plumbing Stoppages</li>
+            <li>Water Heater</li>
+            <li>Whirlpool Bathtub</li>
+            <li>Oven / Range / Stove / Cooktop</li>
+            <li>Dishwasher</li>
+            <li>Built-in Microwave</li>
+            <li>Garbage Disposal</li>
+            <li>Ductwork</li>
+            <li>Garage Door Opener</li>
+            <li>Ceiling and exhaust fans</li>
+          </ul>
+      </div>
+      <div class="col-md-6">
+        <h1>Total Plan: </h1>
+            <h6>Everthing in the basic plan, plus the following: </h6>
+                <ul class="reviewParagraphs">
+                    <li>Clothes Washer</li>
+                    <li>Clothes Dryer</li>
+                    <li>Refrigerator</li>
+                    <li>Air Conditioning System</li>
+          </ul>
+          <h3>Choice Home Warranty of course offers optional coverage on property items like
+pools, vacuum systems, septic systems and more.</h3>
+      </div>
+      <div class="col-md-12">
+        <h1>Dealing with Contractors: </h1>
+          <p class="reviewParagraphs">Choice Home Warranty makes it very easy for their customers to make a service call
+and dispatch a contractor quickly via their automated dispatch technology. When the
+contractor arrives, the customer is required to pay $60 to the contract. This fee is more
+or less standard in the industry</p>
+      </div>
+      <div class="col-md-12">
+        <h1>Conclusion</h1>
+          <p class="reviewParagraphs">Choice Home Warranty is highly focused on their customers and we consider that a
+great starting point for any homeowner. In addition to offering comprehensive plans,
+affordable premiums, we consider their automated dispatch and around-the-clock
+customer service center ensures you have the services you need quickly and easily</p>
+      </div>
+
+  </div>
+  <!--<div class="author_box">
       <img class="author_img" src="http://srkheadshotday.com/wp-content/uploads/Sonya_Dreizler_Headshot_S3709_Crop32.jpg">
       <p>
         Goobie Perkins is a former home warranty sales expert and freelance creative writer. They are very smart and credible, so you should believe whatever they say.
       </p>    
 
-  </div>
+  </div> !-->
 
 
 
